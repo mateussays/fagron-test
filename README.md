2e6ce3bfc68744686385028a1ac38012786ef80b
Edrei <edreimateus@gmail.com>
2021-07-22 14:56:51 -0300

    feat: README file

---
+++ README.md
@@ -0,0 +1,12 @@
+# FAGRON TECH - FRONT-END TEST
+## _AngularJS_
+
+## Tecnologias usadas:
+
+- Angular JS
+- HTML/CSS
+- JS ES6+
+- Node (gerenciador de depêndencia)
+
+
+Este é um repositório para o teste da empresa FagronTech destinado a vaga de desenvolvedor de software front-end jr. 
--- js/controllers/detailCtrl.js
+++ js/controllers/detailCtrl.js
@@ -5,8 +5,6 @@ angular.module('fagronBlog').controller('detailCtrl', function($scope, $http, $r
     url: 'https://jsonplaceholder.typicode.com/posts/' + id
   }).then(function successRequest(response) {
     $scope.detail = response.data;
-    console.log($scope.detail)
-    console.log($route.current.params.id)
   });
 
   $http({
@@ -14,7 +12,5 @@ angular.module('fagronBlog').controller('detailCtrl', function($scope, $http, $r
     url: 'https://jsonplaceholder.typicode.com/comments?postId=' + id
   }).then(function successRequest(response) {
     $scope.comments = response.data;
-    console.log($scope.comments)
-    console.log($route.current.params.id)
   });
 });
